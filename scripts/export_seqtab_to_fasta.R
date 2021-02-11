c4afbd73cee745f14b9640a45106df7c2cc36ba7
Clara Jegousse <clara.jegousse@gmail.com>
2021-02-11 15:01:11 +0000

    Fix seqtab

--- scripts/export_seqtab_to_fasta.R
+++ scripts/export_seqtab_to_fasta.R
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
+# ./scripts/export_seqtab_to_fasta.R \
+# results/dada2/merged/20190508_0074/FX003-016-16S-V4_S58.RDS
+# results/dada2/merged/20190508_0074/FX003-016-16S-V4_S58.fasta
+
 args = commandArgs(trailingOnly=TRUE)
 
 # test if there is at least one argument: if not, return an error
@@ -9,12 +13,15 @@ if (length(args)<2) {
 
 library(dada2)
 
+# args <- c("results/dada2/merged/20190508_0074/FX003-016-16S-V4_S58.RDS", "results/dada2/merged/20190508_0074/FX003-016-16S-V4_S58.fasta")
+
 seqtab <- readRDS(args[1])
 
-asv_seqs <- colnames(seqtab)
-asv_headers <- vector(dim(seqtab)[2], mode="character")
+# asv_seqs <- colnames(seqtab)
+asv_seqs <- seqtab$sequence
+asv_headers <- vector(dim(seqtab)[1], mode="character")
 
-for (i in 1:dim(seqtab)[2]) {
+for (i in 1:dim(seqtab)[1]) {
   asv_headers[i] <- paste(">ASV", i, sep="_")
 }
 
