d5abefb18a257644ff23a340892fb4c174b813c3
Clara Jegousse <clara.jegousse@gmail.com>
2021-02-12 22:17:10 +0000

    Extract fastsa

--- scripts/extract_dada2_results.R
+++ scripts/extract_dada2_results.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 # ./scripts/extract_dada2_results.R "results/dada2/seqtab/20190508_0074/seqtab.nochimeras.RDS" \
-"results/dada2/taxa/20190508_0074/taxa.RDS" \
 "results/dada2/final/20190508_0074-ASVs.fa" \
 "results/dada2/final/20190508_0074-ASVs_counts.tsv"
 
@@ -10,7 +9,6 @@ args = commandArgs(trailingOnly=TRUE)
 library(dada2)
 
 # args <- c("results/dada2/seqtab/20190508_0074/seqtab.nochimeras.RDS",
-  "results/dada2/taxa/20190508_0074/taxa.RDS",
   "results/dada2/final/20190508_0074-ASVs.fa",
   "results/dada2/final/20190508_0074-ASVs_counts.tsv")
 
@@ -28,9 +26,9 @@ for (i in 1:dim(seqtab)[2]) {
 
 # making and writing out a fasta of our final ASV seqs:
 asv_fasta <- c(rbind(asv_headers, asv_seqs))
-write(asv_fasta, file = args[3])
+write(asv_fasta, file = args[2])
 
 # count table:
 asv_tab <- t(seqtab)
 row.names(asv_tab) <- sub(">", "", asv_headers)
-write.table(asv_tab, args[4], sep="\t", quote=F, col.names=NA)
+write.table(asv_tab, args[3], sep="\t", quote=F, col.names=NA)
