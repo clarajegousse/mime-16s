5b1b6cfef093039690769fcf0d6d10488075ff95
Clara Jegousse <clara.jegousse@gmail.com>
2021-02-12 10:25:20 +0000

    Update config samples.csv

--- scripts/rename_files.sh
+++ scripts/rename_files.sh
@@ -1,13 +1,19 @@
-for i in `ls *R1*.fastq.gz`; do
-  filename=$(echo $i)
-  samplename=$(echo $i | cut -f 1 -d "_")
-  newfilename=$(echo $samplename".1.fastq.gz" )
-  mv $filename $newfilename
-done
+cd /users/home/cat3/projects/mime-16s/data/miseq/20190508_0074
 
-for i in `ls *R2*.fastq.gz`; do
-  filename=$(echo $i)
+echo "Sample, R1, R2" > samples.csv
+for i in `ls *R1*.fastq.gz`; do
+
+  fwd_filename=$(echo $i)
+  rev_filnemae=$(echo $i | sed 's/R1/R2/')
+
   samplename=$(echo $i | cut -f 1 -d "_")
-  newfilename=$(echo $samplename".2.fastq.gz" )
-  mv $filename $newfilename
+
+  mv $fwd_filename $fwd_new_filename
+  mv $rev_filnemae $rev_new_filename
+
+  fwd_new_filename=$(echo $samplename"_R1.fastq.gz" )
+  rev_new_filename=$(echo $samplename"_R2.fastq.gz" )
+
+  echo "$samplename, $fwd_new_filename, $rev_new_filename" >> samples.csv
+
 done
