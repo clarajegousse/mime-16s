be48239c36cd9b97cdd6341cdb2fced106e0d40d
Clara Jegousse <clara.jegousse@gmail.com>
2021-02-15 13:24:22 +0000

    With argparser

--- scripts/dada2-filter.r
+++ scripts/dada2-filter.r
@@ -2,31 +2,33 @@
 # dada2-filter.R input_path output_path trunc_len maxee truncq
 
 # args = commandArgs(trailingOnly=TRUE)
-suppressPackageStartupMessages(library("argparse"))
+library(argparser, quietly=TRUE)
 
-# create parser object
-parser <- ArgumentParser()
+library(argparser, quietly=TRUE)
 
-# specify our desired options
-# by default ArgumentParser will add an help option
-parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
-    help="Print extra output [default]")
-parser$add_argument("-i", "--input_path", action="store_true", default=FALSE,
-  help="input path [default: none]")
-parser$add_argument("-o", "--output_path", action="store_true", default=FALSE,
-  help="output path [default: none]")
-parser$add_argument("-f", "--trunclenfwd", type="integer", action="store_true", default=250,
-  help="truncLen forward [default %(default)s]")
-parser$add_argument("-r", "--trunclenrev", type="integer", action="store_true", default=200,
-  help="truncLen reverse [default %(default)s]")
-parser$add_argument("-m", "--maxee", type="integer", action="store_true", default=2,
-    help="MaxEE [default %(default)s]")
-parser$add_argument("-t", "--truncq", type="integer", action="store_true", default=11,
-        help="TruncQ [default %(default)s]")
+# Create a parser
+p <- arg_parser("Round a floating point number")
 
-# get command line options, if help option encountered print help and exit,
-# otherwise if options not found on command line then set defaults,
-args <- parser$parse_args()
+# input_path <- args[1]
+# output_path <- args[2]
+# trunc.len.fwd <- as.numeric(args[3])
+# trunc.len.rev <- as.numeric(args[4])
+# maxee <- as.numeric(args[5])
+# truncq <- as.numeric(args[6])
+
+# Create a parser
+p <- arg_parser("Run DADA2 filter")
+
+# Add command line arguments
+p <- add_argument(p, "--input_path", help="Input path", default=FALSE)
+p <- add_argument(p, "--output_path", help="Output path", default=FALSE)
+p <- add_argument(p, "--trunc_len_fwd", help="number of decimal places",  type="numeric", default=250)
+p <- add_argument(p, "--trunc_len_rev", help="number of decimal places",  type="numeric", default=200)
+p <- add_argument(p, "--maxee", help="MaxEE",  type="numeric", default=2)
+p <- add_argument(p, "--truncq", help="truncQ",  type="numeric", default=11)
+
+# Parse the command line arguments
+argv <- parse_args(p)
 
 # ----- LIBRARY -----
 
@@ -45,11 +47,11 @@ library(dada2); packageVersion("dada2")
 # truncq <- as.numeric(args[6])
 
 # File parsing
-pathF <- input_path
-pathR <- input_path
+pathF <- argv$input_path
+pathR <- argv$input_path
 
-filtpathF <- file.path(output_path)
-filtpathR <- file.path(output_path)
+filtpathF <- file.path(argv$output_path)
+filtpathR <- file.path(argv$output_path)
 
 fastqFs <- sort(list.files(pathF, pattern="R1.fastq.gz"))
 fastqRs <- sort(list.files(pathR, pattern="R2.fastq.gz"))
@@ -60,6 +62,6 @@ filterAndTrim(fwd = file.path(pathF, fastqFs),
   filt = file.path(filtpathF, fastqFs),
   rev = file.path(pathR, fastqRs),
   filt.rev = file.path(filtpathR, fastqRs),
-  truncLen = c(trunc.len.fwd, trunc.len.rev),
-  maxEE = maxee, truncQ = truncq
+  truncLen = c(argv$trunc_len_fwd, argv$trunc_len_rev),
+  maxEE = argv$maxee, truncQ = argv$truncq
   compress = TRUE, verbose = TRUE, multithread = TRUE)
