3ea34116ed284f9170b05a16e0977cb25c8a56e0
Clara Jegousse <clara.jegousse@gmail.com>
2021-02-16 13:21:48 +0000

    Fix slash in path

--- scripts/dada2-merge-chimera-taxo.r
+++ scripts/dada2-merge-chimera-taxo.r
@@ -35,5 +35,6 @@ tax <- assignTaxonomy(seqtab.nochim, "/users/work/cat3/db/dada2/silva_nr99_v138_
 
 # Write to disk
 ifelse(!dir.exists(file.path(argv$output_path)), dir.create(file.path(argv$output_path)), FALSE)
-saveRDS(seqtab.nochim, paste(argv$output_path, "seqtab_final.rds", sep = "", collpase = TRUE)) # CHANGE ME to where you want sequence table saved
-saveRDS(tax, paste(argv$output_path, "tax_final.rds", sep = "", collapse = NULL))
+
+saveRDS(seqtab.nochim, paste(argv$output_path, "/seqtab_final.rds", sep = "", collpase = TRUE)) # CHANGE ME to where you want sequence table saved
+saveRDS(tax, paste(argv$output_path, "/tax_final.rds", sep = "", collapse = NULL))
