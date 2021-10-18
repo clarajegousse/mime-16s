8e48767efa40e52b3b269babf9ac7c54adbadea3
Clara <clara.jegousse@gmail.com>
2021-10-18 09:23:35 +0000

    Create colours-frontiers.r

---
+++ visuals/colours-frontiers.r
@@ -0,0 +1,42 @@
+# Colours for R
+# Clara Jégousse
+
+# ----- Color definitions -----
+
+grapefruit <- "#D03F57"
+Grapefruit <- "#C9243F"
+bittersweet <- "#DB4747"
+Bittersweet <- "#D21919" #
+orange <- "#F5A833"
+Orange <- "#F39200"
+sunflower <- "#F2DD33"
+Sunflower <- "#EFD500"
+grass <- "#AACD4C"
+Grass <- "#95C11F"
+mint <- "#5AC5C9"
+Mint <- "#31B7BC"
+tree <- "#339564"
+Tree <- "#007B3D"
+aqua <- "#33A9D7"
+Aqua <- "#0094CD"
+jeans <- "#4567A9"
+Jeans <- "#164194"
+lavender <- "#AC92EC"
+Lavender <- "#967ADC"
+eggplant <- "#8C5388"
+Eggplant <- "#6F286A"
+rose <- "#EC87C0"
+Rose <- "#D770AD"
+lightgrey <- "#D6D5D5"
+LightGrey <- "#B8B7B7"
+mediumgrey <- "#8D8C8C"
+MediumGrey <- "#706F6F"
+darkgrey <- "#606060"
+DarkGrey <- "#383838"
+chocolate <- "#A8714D"
+Chocolate <- "#9C5D34"
+Skin <- "#F1CFAE"
+
+frontiers.palette <- c(Grapefruit, Bittersweet,
+  Orange, Sunflower, Grass, Mint, Tree, Aqua, Jeans, Eggplant,
+  LightGrey, MediumGrey, DarkGrey, Chocolate, Skin)
